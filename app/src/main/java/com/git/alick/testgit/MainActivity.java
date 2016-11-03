d487df3342f799277e33679e1f9ce995dcedbf37
wolongalick <1607009565@qq.com>
2016-11-03 10:54:37 +0800

    sdfds

--- app/src/main/java/com/git/alick/testgit/MainActivity.java
+++ app/src/main/java/com/git/alick/testgit/MainActivity.java
@@ -10,24 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-        System.out.print("cxw");
-        System.out.print("cxw");
         System.out.print("cxw");
 
-
-
-        System.out.print("mv");
-        System.out.print("mv");
-
-
-
-
-=======
->>>>>>> 3c8dd91de4dad215d83dbb8c6d6cbfce43abb8ff
-        System.out.print("mv");
-        System.out.print("mv");
-
+        //fuwuqo
 
 
     }

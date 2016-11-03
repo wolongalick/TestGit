37622ce4f7e113318f60396e99245d22f8d6208f
wolongalick <1607009565@qq.com>
2016-11-03 10:38:05 +0800

    sdfds

--- app/src/main/java/com/git/alick/testgit/MainActivity.java
+++ app/src/main/java/com/git/alick/testgit/MainActivity.java
@@ -24,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.print("mv");
         System.out.print("mv");
-        System.out.print("mv");
-        System.out.print("mv");
 
 
 

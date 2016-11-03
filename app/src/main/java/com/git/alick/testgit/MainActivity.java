12c79bdaf5925db24a5d1ab7bc716e314f8cb356
wolongalick <1607009565@qq.com>
2016-11-03 10:28:13 +0800

    sdfds

--- app/src/main/java/com/git/alick/testgit/MainActivity.java
+++ app/src/main/java/com/git/alick/testgit/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.print("cxw");
+        System.out.print("cxw");
+        System.out.print("cxw");
+
+
     }
 }

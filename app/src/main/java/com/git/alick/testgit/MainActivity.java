3a5fff5d0202727e7046094d5dab89798c7b31ae
wolongalick <1607009565@qq.com>
2016-11-03 10:36:04 +0800

    sdfds

--- app/src/main/java/com/git/alick/testgit/MainActivity.java
+++ app/src/main/java/com/git/alick/testgit/MainActivity.java
@@ -11,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.print("mv");
+        System.out.print("mv");
+        System.out.print("mv");
+        System.out.print("mv");
+
+
+
     }
 }
